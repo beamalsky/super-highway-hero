4ef702564c4d4be2879cccd24c5af2ef3b3f2b59
apidae <beamalsky@gmail.com>
2017-03-06 12:21:16 -0600

    Unity setup

---
+++ smart-highway-hero/Smart Highway Hero/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public GameObject player;
+	public float speed;
+	private Vector3 offset;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = new Vector3 (0.0f, 0.0f, speed);
+	}
+}
---
+++ smart-highway-hero/Smart Highway Hero/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax;	
+}
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private Transform tf;
+	public Boundary boundary;
+
+	public float laneWidth;
+	public float speed;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		tf = GetComponent<Transform> ();
+		rb.velocity = new Vector3 (0.0f, 0.0f, speed);
+	}
+
+	void Update () {
+		
+		if (Input.GetKeyDown("z")) {
+			tf.position = new Vector3 (tf.position.x - laneWidth, tf.position.y, tf.position.z);
+		}
+
+		if (Input.GetKeyDown("x")) {
+			tf.position = new Vector3 (tf.position.x + laneWidth, tf.position.y, tf.position.z);
+		}
+
+		tf.position = new Vector3 (
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), 
+			2.0f, 
+			tf.position.z
+		);
+	}
+}
